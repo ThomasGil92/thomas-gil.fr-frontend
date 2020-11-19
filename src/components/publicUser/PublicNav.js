a6a8aee3997c81376e847f70cb33ab61c32bcae5
ThomasGil92 <tgil849@gmail.com>
2020-11-19 15:27:03 +0100

    deploy

--- src/components/publicUser/PublicNav.js
+++ src/components/publicUser/PublicNav.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
 
 const PublicNav = ({ language, setLanguage }) => {
